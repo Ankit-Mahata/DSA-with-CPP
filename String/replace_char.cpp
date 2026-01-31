ced32760361553c2ae411af28c8353d70077dadf
Ankit-Mahata <ankitmahata44@gmail.com>
2026-01-31 16:16:48 +0545

    replacing specific character with a string

--- String/replace_char.cpp
+++ String/replace_char.cpp
@@ -1,6 +1,28 @@
-
+// to replace the occuranace of a specif character with a word
+/* if input="ankit" , char= a , and replacing string is ram then output is 
+ramkit */
 #include<iostream>
+#include<cstring>
 using namespace std;
+void replace_char(char *str1, char *str2, char chr)
+{
+    int len1=strlen(str1);
+    int len2=strlen(str2);
+    if(len1==0)
+    return;
+    if(str1[0]==chr)
+    {
+        for(int i=len1; str1[i]!=chr; i--)
+        {
+            str1[i+(len2-1)]=str1[i];
+        }
+        for(int i=0; i<len2; i++)
+        {
+            str1[i]=str2[i];
+        }
+    }
+    replace_char(str1+1,str2,chr);
+}
 int main()
 {
     char str1[20];
@@ -12,5 +34,6 @@ int main()
     cin>>chr;
     cout<<"\ngive a string that replace the character : ";
     cin>>str2;
-
+    replace_char(str1,str2,chr);
+    cout<<"\n updated string is : "<<str1;
 }
